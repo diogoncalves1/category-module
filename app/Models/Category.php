76052d77794050e03622369e7342eb5f19c9dc1d
diogogoncalves <dmgdark95@gmail.com>
2025-08-06 11:44:18 +0100

    Category Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,18 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\User\Entities\User;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
 
-    protected $table = 'category';
+    protected $table = 'categories';
     protected $fillable = ['info', 'type', 'icon', 'color', 'default', 'parent_id', 'user_id'];
 
     public function user()
     {
-        // return $this->belongsTo();
+        return $this->belongsTo(User::class);
     }
 
     public function parent()
