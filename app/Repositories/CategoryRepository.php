ba6da1376faf42a7222ee5278f8388a420d87f15
diogogoncalves <dmgdark95@gmail.com>
2025-08-09 16:13:29 +0100

    Repository

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -2,6 +2,10 @@
 
 namespace App\Repositories;
 
+use App\Exceptions\CannotDeleteDefaultCategoryException;
+use App\Exceptions\CannotDeleteOthersCategoryException;
+use App\Exceptions\CannotUpdateDefaultCategoryException;
+use App\Exceptions\CannotUpdateOthersCategoryException;
 use App\Exceptions\UnauthorizedDefaultCategoryException;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -16,18 +20,34 @@ class CategoryRepository implements RepositoryInterface
         return Category::all();
     }
 
+    public function allUser()
+    {
+        $categories =  Category::default(1)
+            // ->orWhere('user_id', auth()->user()->id)
+            ->get();
+
+        $userLang = /* $_COOKIE['lang'] ?? */ 'en';
+
+        foreach ($categories as &$category) {
+            $category->name = json_decode($category->name)->{$userLang}  ?? json_decode($category->name);
+        }
+
+        return $categories;
+    }
+
     public function store(Request $request)
     {
         try {
             return DB::transaction(function () use ($request) {
-                $input = $request->only(['info', 'type', 'icon', 'color', 'parent_id']);
+                $input = $request->only(['type', 'icon', 'color', 'parent_id']);
                 $user = Auth::user();
+                $input["name"] = json_encode($request->get('name'));
 
                 if (!$request->get('default')) {
-                    $input['user_id'] = $user->id;
+                    // $input['user_id'] = $user->id;
                     $input['default'] = 0;
                 } else {
-                    if (!$user->can('createDefaultCategory'))
+                    if (!$user || !$user->can('createDefaultCategory'))
                         throw new UnauthorizedDefaultCategoryException();
                     $input['default'] = 1;
                 }
@@ -35,11 +55,13 @@ class CategoryRepository implements RepositoryInterface
                 $category = Category::create($input);
 
                 Log::info('Category ' . $category->id . ' successfully created.');
-                return response()->json(["success" => true, "message" => __('')]);
+                return response()->json(["success" => true, "message" => __('alerts.categoryAdded')]);
             });
         } catch (\Exception $e) {
             Log::error($e);
-            return response()->json(['error' => true, "message" => $e->getMessage()], $e->getCode());
+            if ($e->getCode())
+                return response()->json(['error' => true, "message" => $e->getMessage()], $e->getCode());
+            return response()->json(['error' => true, 'message' => __('alerts.errorAddCategory')], 500);
         }
     }
 
@@ -49,19 +71,27 @@ class CategoryRepository implements RepositoryInterface
             return DB::transaction(function () use ($request, $id) {
                 $category = $this->show($id);
 
-                $input = $request->only('info', 'type', 'icon', 'color', 'parent_id');
+                $input = $request->only(['type', 'icon', 'color', 'parent_id']);
+
+                $input["name"] = json_encode($request->get('name'));
 
                 $user = Auth::user();
 
-                if (!$category->default && $category->user_id != $user->id || !$user->can('createDefaultCategory'))
-                    throw new UnauthorizedDefaultCategoryException();
+                // if (!$user || !$category->default  && $category->user_id != $user->id)
+                // throw new CannotUpdateOthersCategoryException();
+
+                if ($category->default && !$user->can('updateDefaultCategory'))
+                    throw new CannotUpdateDefaultCategoryException();
 
                 $category->update($input);
 
-                return response()->json(['success' => true, 'message' => __('frontend.')]);
+                return response()->json(['success' => true, 'message' => __('alerts.categoryUpdated')]);
             });
         } catch (\Exception $e) {
             Log::error($e);
+            if ($e->getCode())
+                return response()->json(['error' => true, "message" => $e->getMessage()], $e->getCode());
+            return response()->json(['error' => true, 'message' => __('alerts.errorUpdateCategory')], 500);
         }
     }
 
@@ -71,17 +101,22 @@ class CategoryRepository implements RepositoryInterface
             return DB::transaction(function () use ($id) {
                 $category = $this->show($id);
 
-                $user = Auth::user();
+                // $user = Auth::user();
 
-                if (!$category->default && $category->user_id != $user->id || !$user->can('createDefaultCategory'))
-                    throw new UnauthorizedDefaultCategoryException();
+                // if (!$user || !$category->default  && $category->user_id != $user->id)
+                //     throw new CannotDeleteOthersCategoryException();
+                if ($category->default && !$user->can('destroyDefaultCategory'))
+                    throw new CannotDeleteDefaultCategoryException();
 
                 $category->delete();
 
-                response()->json(["success" => true, "message" => __('')]);
+                return response()->json(["success" => true, "message" => __('alerts.categoryDeleted')]);
             });
         } catch (\Exception $e) {
             Log::error($e);
+            if ($e->getCode())
+                return response()->json(['error' => true, "message" => $e->getMessage()], $e->getCode());
+            return response()->json(["success" => true, "message" => __('alerts.errorDeleteCategory')], 500);
         }
     }
 
@@ -118,18 +153,18 @@ class CategoryRepository implements RepositoryInterface
         $categories = $query->offset($request->start)
             ->limit($request->length)
             ->default($request->get('default'))
-            ->select("id", 'info as name', 'info', 'parent_id', "type", 'icon', 'color')
+            ->select("id", 'name', 'parent_id', "type", 'icon', 'color')
             ->get();
 
         $total = $query->count();
 
         foreach ($categories as &$category) {
             $category->name = ($category->default) ?
-                optional(json_decode($category->info))->{$userLang}->name : optional(json_decode($category->info))->name;
+                json_decode($category->name)->{$userLang} : json_decode($category->name);
 
             if ($category->parent)
                 $category->parentName = (optional($category->parent)->default) ?
-                    json_decode($category->parent->info)->{$userLang}->name : json_decode($category->parent->info)->name;
+                    json_decode($category->parent->name)->{$userLang} : json_decode($category->parent->name);
             else
                 $category->parentName = null;
 
@@ -138,7 +173,7 @@ class CategoryRepository implements RepositoryInterface
                                     <a type='button' href='" . route('categories.edit', $category->id) . "' class='btn mr-1 btn-default'>
                                         <i class='fas fa-edit'></i>
                                     </a>
-                                    <button type='button' onclick='modalDelete({$category->id})' class='btn btn-default'>
+                                    <button type='button' onclick='modalDelete(`" . route('api.categories.destroy', $category->id) . "`)' class='btn btn-default'>
                                         <i class='fas fa-trash'></i>
                                     </button>
                                   </div>";
