5c161c8da7264c0f0f4f04d8a5746bed4acac793
diogogoncalves <dmgdark95@gmail.com>
2025-10-31 19:09:56 +0000

    Category Module Exceptions

--- Modules/Category/Exceptions/CannotDeleteOthersCategoryException.php
+++ Modules/Category/Exceptions/CannotDeleteOthersCategoryException.php
@@ -6,8 +6,11 @@ use Exception;
 
 class CannotDeleteOthersCategoryException extends Exception
 {
+    protected $code = 403;
+    protected $message;
+
     public function __construct()
     {
-        parent::__construct(__('alerts.cannotDeleteOthersCategoryException'), 403);
+        parent::__construct(__('category::exceptions.categories.cannotDeleteOthersCategoryException'), $this->code);
     }
 }
--- Modules/Category/Exceptions/CannotUpdateOthersCategoryException.php
+++ Modules/Category/Exceptions/CannotUpdateOthersCategoryException.php
@@ -6,8 +6,11 @@ use Exception;
 
 class CannotUpdateOthersCategoryException extends Exception
 {
+    protected $code = 403;
+    protected $message;
+
     public function __construct()
     {
-        parent::__construct(__('alerts.cannotUpdateOthersCategoryException'), 403);
+        parent::__construct(__('category::exceptions.categories.cannotUpdateOthersCategoryException'), $this->code);
     }
 }
