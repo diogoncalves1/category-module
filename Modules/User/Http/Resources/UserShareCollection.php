f32f6475eaa6f65b378fdcfa20a970b44d79d436
diogogoncalves <dmgdark95@gmail.com>
2025-11-17 21:40:41 +0000

    Update user module to version 1.1.2

--- Modules/User/Http/Resources/UserShareCollection.php
+++ Modules/User/Http/Resources/UserShareCollection.php
@@ -1,17 +1,16 @@
 <?php
-
 namespace Modules\User\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class UserCollection extends ResourceCollection
+class UserShareCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
      */
     public function toArray(Request $request)
     {
-        return UserResource::collection($this->collection);
+        return UserShareResource::collection($this->collection);
     }
 }
